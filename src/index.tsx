8f9a11028bdf8e9d1e9d706871fe628c8c69ba40
Jiheon Kim <siwmua0@naver.com>
2024-04-18 04:13:41 +0900

    pnpm, vite 환경으로 다시 셋업

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import router from "./routes";
-
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<RouterProvider router={router} />);
