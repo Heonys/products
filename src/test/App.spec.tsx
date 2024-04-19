1d5292e05449c2e71d2b51d6490b4ab27cb18957
Jiheon Kim <siwmua0@naver.com>
2024-04-20 05:52:03 +0900

    vitest, testring-library 셋업

---
+++ src/test/App.spec.tsx
@@ -0,0 +1,8 @@
+// import { screen } from '@testing-library/react';
+// import render from './render';
+// import { ProductsList } from '@/pages';
+
+it('페이지 로드시 10개의 상품이 보여진다', async () => {
+  // render(<ProductsList />);
+  // expect(await screen.findByPlaceholderText('상품을 입력해주세요')).toBeInTheDocument();
+});
